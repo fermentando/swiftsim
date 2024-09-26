a7e0d65c8fdfcb61cadbf8b1017db6f0a09dd49c
fermentando <fernando.hidalgo.pineda@gmail.com>
2024-09-26 19:28:58 +0100

    Original firehose model

--- src/feedback/KIARA/KIARA/feedback_debug.h
+++ src/feedback/KIARA/KIARA/feedback_debug.h
@@ -1,7 +1,8 @@
 /*******************************************************************************
  * This file is part of SWIFT.
  * Copyright (c) 2022 Bert Vandenbroucke (bert.vandenbroucke@gmail.com)
- *
+ *               2022 Doug Rennehan (douglas.rennehan@gmail.com)
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
@@ -16,25 +17,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  ******************************************************************************/
-#ifndef SWIFT_CHEMISTRY_KIARA_DEBUG_H
-#define SWIFT_CHEMISTRY_KIARA_DEBUG_H
+#ifndef SWIFT_FEEDBACK_KIARA_DEBUG_H
+#define SWIFT_FEEDBACK_KIARA_DEBUG_H
 
-__attribute__((always_inline)) INLINE static void chemistry_debug_particle(
-    const struct part* p, const struct xpart* xp) {
+__attribute__((always_inline)) INLINE static void feedback_debug_particle(
+    const struct part* p, const struct xpart* xp) {}
 
-  warning("[PID%lld chemistry_part_data:", p->id);
-  for (int i = 0; i < chemistry_element_count; i++) {
-    warning("[PID%lld metal_mass_fraction[%i]: %.3e", p->id, i,
-            p->chemistry_data.metal_mass_fraction[i]);
-  }
-  warning("[PID%lld metal_mass_fraction_total: %.3e", p->id,
-          p->chemistry_data.metal_mass_fraction_total);
-  for (int i = 0; i < chemistry_element_count; i++) {
-    warning("[PID%lld metal_mass_fraction[%i]: %.3e", p->id, i,
-            p->chemistry_data.metal_mass_fraction[i]);
-  }
-  warning("[PID%lld metal_mass_fraction_total: %.3e", p->id,
-          p->chemistry_data.metal_mass_fraction_total);
-}
-
-#endif /* SWIFT_CHEMISTRY_KIARA_DEBUG_H */
+#endif /* SWIFT_FEEDBACK_KIARA_DEBUG_H */
